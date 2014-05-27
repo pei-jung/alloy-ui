2370bc73918bd824471ac34782baebd926ec53b6
Pei-Jung Lan <pei-jung.lan@liferay.com>
2014-05-27 15:24:56 -0700

    LPS-1336 Add test case to unit test

--- src/aui-modal/tests/unit/js/tests.js
+++ src/aui-modal/tests/unit/js/tests.js
@@ -143,8 +143,58 @@ YUI.add('aui-modal-tests', function(Y) {
 
     }));
 
+    //--------------------------------------------------------------------------
+    // Test Case for Scroll
+    //--------------------------------------------------------------------------
+
+    suite.add(new Y.Test.Case({
+
+        name: 'Scroll',
+
+        setUp: function() {
+            if (modal) {
+                modal.destroy();
+            }
+
+            modal = new Y.Modal(
+                {
+                    parentScrollable: false
+                }
+            ).render('#modal');
+
+            boundingBox = modal.get('boundingBox');
+        },
+
+        tearDown: function() {
+            modal.destroy();
+
+            modal = null;
+            boundingBox = null;
+        },
+
+        //----------------------------------------------------------------------
+        // Tests
+        //----------------------------------------------------------------------
+
+        // Tests: AUI-1336
+        'check scroll-disabled after visibleChange with parentScrollable disabled': function() {
+            var body = Y.getBody(),
+                documentEl = Y.getDoc().get('documentElement');
+
+            modal.show();
+
+            Y.Assert.isTrue(body.hasClass('scroll-disabled'));
+            Y.Assert.isTrue(documentEl.hasClass('scroll-disabled'));
+
+            modal.hide();
+
+            Y.Assert.isFalse(body.hasClass('scroll-disabled'));
+            Y.Assert.isFalse(documentEl.hasClass('scroll-disabled'));
+        }
+    }));
+
     Y.Test.Runner.add(suite);
 
 }, '', {
-    requires: ['aui-modal', 'node-event-simulate', 'test']
+    requires: ['aui-modal', 'aui-node-base','node-event-simulate', 'test']
 });
